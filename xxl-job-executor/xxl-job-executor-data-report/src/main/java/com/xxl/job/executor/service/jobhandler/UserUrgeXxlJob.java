120ec123bed0b057b0233f0b124590e4e8d00c97
Turtle <bu7tzgqqgnp3znyb2tvkikyuqvf2vvobmrij6vlcb273dmtuuj4q>
2022-03-15 14:42:14 +0800

    add : user urge

--- xxl-job-executor/xxl-job-executor-data-report/src/main/java/com/xxl/job/executor/service/jobhandler/UserUrgeXxlJob.java
+++ xxl-job-executor/xxl-job-executor-data-report/src/main/java/com/xxl/job/executor/service/jobhandler/UserUrgeXxlJob.java
@@ -2,6 +2,8 @@ package com.xxl.job.executor.service.jobhandler;
 
 import com.xxl.job.core.context.XxlJobHelper;
 import com.xxl.job.core.handler.annotation.XxlJob;
+import com.yicall.sso.rpc.service.RpcUserUrgeService;
+import org.apache.dubbo.config.annotation.DubboReference;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,12 +16,42 @@ import org.springframework.stereotype.Component;
 public class UserUrgeXxlJob {
     private static final Logger log = LoggerFactory.getLogger(UserUrgeXxlJob.class);
 
+    @DubboReference(version = "1.0.0")
+    private RpcUserUrgeService rpcUserUrgeService;
+
     @XxlJob("morningHandler")
     public void morningHandler(){
-        XxlJobHelper.log("催报开始执行");
-        XxlJobHelper.handleFail("测试失败");
-        log.info("催报执行");
-        XxlJobHelper.log("催报执行结束");
+        try{
+            rpcUserUrgeService.morningExecute();
+        }catch (Exception e){
+            XxlJobHelper.handleFail("上午催报定时异常");
+        }
     }
 
+    @XxlJob("nightHandler")
+    public void nightHandler(){
+        try{
+            rpcUserUrgeService.nightExecute();
+        }catch (Exception e){
+            XxlJobHelper.handleFail("晚上催报定时异常");
+        }
+    }
+
+    @XxlJob("everyMonthHandler")
+    public void everyMonthHandler(){
+        try{
+            rpcUserUrgeService.everyMonthExecute();
+        }catch (Exception e){
+            XxlJobHelper.handleFail("每周一催报定时异常");
+        }
+    }
+
+    @XxlJob("customizeHandler")
+    public void customizeHandler(){
+        try{
+            rpcUserUrgeService.customizeExecute();
+        }catch (Exception e){
+            XxlJobHelper.handleFail("自定义催报定时异常");
+        }
+    }
 }
